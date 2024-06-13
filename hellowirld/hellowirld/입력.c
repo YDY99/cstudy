da2024dc6ea418cb62bb1238e46109ebf26e591b
YDY99 <eodud1080@naver.com>
2024-06-13 17:45:25 +0900

    6월13일 변수 num

---
+++ hellowirld/hellowirld/입력.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int Main(void)
+{
+	int width = 24, height = 12, length = 8;
+
+	int num;
+	printf("숫자를 입력해주세요 ＼n");
+	scanf_s("%d", & num);
+	
+	printf("num의 들어간 숫자 : %d", num);
+	int num1  = 3 ;
+	int num2  = 2;
+	printf num1 + num2;
+	printf("첫번쨰 정수를 입력해주세요");
+	scanf_S(" %d, 3 ");
+	printf("두번쨔 정수를 입력하세요");
+	scanf_s(" %d 2");
+	printf 
+	
+	printf("%d ＼n", num);
+
+	printf 
+	return 0;
+
+}
