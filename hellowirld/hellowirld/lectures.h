2f4806a1154b2867a01de0fb14fcdebc4dcf73a0
YDY99 <eodud1080@naver.com>
2024-06-18 17:43:23 +0900

    6월18일

--- hellowirld/hellowirld/lectures.h
+++ hellowirld/hellowirld/lectures.h
@@ -6,9 +6,11 @@
 // #include <파일이름> : 시스템경로에서 해당 파일이 있는지 없는지 확인 한다
 // #include "파일이름" : 사용자가 만든 헤더 파일을 먼저 검사하고, 만약에 없으면 시스템 경로에서 가져 찾습니다
 
-void lectures1();
-void lectures2();
-void lectures3();
-void lectures4();
-void lectures5();
+//void lectures1();
+//void lectures2();
+//void lectures3();
+//void lectures4();
+//void lectures5();
+
+void lecture7();
 
--- hellowirld/hellowirld/main.c
+++ hellowirld/hellowirld/main.c
@@ -5,7 +5,7 @@ int main(void)
 	//lectures1();
 	//lectures2();
 	//lectures3();
-	lectures4();
+	//lectures4();
 
 
 	return 0;
