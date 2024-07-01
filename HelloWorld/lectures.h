0b7971a4378a9b11141f182fe2ee4eff7126f3e3
YDY99 <eodud1080@naver.com>
2024-07-01 16:26:43 +0900

    수업코드 아플때

---
+++ HelloWorld/lectures.h
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+void lecture1();
+void lecture2();
+void lecture3();
+void lecture4();
+void lecture4_2();
+void lecture5();
+void lecture6();
+void lecture7();
+void lecture8();
+void lecture09();
+void lecture10();
+void lecture11();
+/// <summary>
+/// 강의12. 반복문
+/// </summary>
+void lecture12();
+/// <summary>
+/// 반복문 예제 문제
+/// </summary>
+void lecture12_2();
+/// <summary>
+/// while 반복문
+/// </summary>
+void lecture12_3();
+/// <summary>
+/// switch 조건문
+/// </summary>
+void lecture13();
+void lecture13_2();
+void lecture14();
+void lecture15();
---
+++ HelloWorld/메인.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "lectures.h"
+
+int main(void) 
+{
+	//lecture1();
+	//lecture2();
+	//lecture3();
+	//lecture4();
+	//lecture4_2();
+	//lecture5();
+	//lecture6();
+	//lecture7();
+	//lecture8();
+	//lecture09();
+	//lecture10();
+	//lecture11();
+	//lecture12();
+	//lecture12_2();
+	//lecture12_3();
+	//lecture13();
+	//lecture13_2();
+	//lecture14();
+	lecture15();
+	return 0;
+}
